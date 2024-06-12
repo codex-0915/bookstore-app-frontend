a5929d078fec37a3ce393d8e337352a74200a47e
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2024-06-13 07:36:50 +0800

    Add api and book services

---
+++ src/app/services/books.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+
+  constructor(private apiService: ApiService) { }
+
+  getBooks= (url: string, params: any): Observable<any> => {
+    return this.apiService.get(url, params);
+  }
+}
